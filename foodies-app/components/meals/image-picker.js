8a79536de14db8374f26654dbdf4b2142be9e859
DJ94DJ <unistudio94@gmail.com>
2024-05-21 21:15:08 +0900

    :sparkles: image-picker 미리보기 구현

--- foodies-app/components/meals/image-picker.js
+++ foodies-app/components/meals/image-picker.js
@@ -1,18 +1,40 @@
 'use client';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import Image from 'next/image';
+
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
   function handlePickClick() {
     imageInput.current.click();
   }
 
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No Image picked yet.</p>}
+          {pickedImage && <Image src={pickedImage} alt="The Image selected by the user." layout="fill" />}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -20,8 +42,9 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          multiple
+          onChange={handleImageChange}
         />
-
         <button className={classes.button} type="button" onClick={handlePickClick}>
           Pick an Image
         </button>
